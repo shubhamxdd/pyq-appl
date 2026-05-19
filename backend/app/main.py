aa26e3bb7ff6177c8259faa9482dfa533aefeff8
shubhamxdd <ssisodia128@gmail.com>
2026-05-19 21:03:18 +0000

    vps changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="PYQ Solver API")
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=[settings.FRONTEND_URL],
-    allow_origins=["*"],  # Allow all origins for development; restrict in production
+    allow_origins=["pyq.shubhamxd.in"],  # Allow all origins for development; restrict in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
